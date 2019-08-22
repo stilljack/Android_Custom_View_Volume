ccaba5a63bebc25eed0ae521a8fc3fd31615edd0
stilljack <stilljack@gmail.comconfig --global user.email stilljack@gmail.com>
2019-08-22 16:53:43 -0700

    prototype of tool is in reasonable condition. TODO NEXT: get button into custom type and out of xml/mainactivity attempt to enforce fitXY if possible, should be.

--- app/src/main/java/com/example/volumeknob/VolumeKnob.kt
+++ app/src/main/java/com/example/volumeknob/VolumeKnob.kt
@@ -8,13 +8,14 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import android.widget.ImageView
 
 
-
-class VolumeKnob(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+class VolumeKnob(context: Context?, attrs: AttributeSet?) : ImageView(context, attrs) {
 
     private val paint = Paint()
     private val paint2 = Paint()
+    private val paint3 = Paint()
 //constructor(context: Context): this(context, null)
     //
     // POSSIBLE ALTERNATE FOR VOLUMEKNOB(context: Context, attrs: AttributeSet, defStyleAttr : Int, defStyleRes: Int)
@@ -33,6 +34,8 @@ class VolumeKnob(context: Context?, attrs: AttributeSet?) : View(context, attrs)
 
     override fun onDraw(canvas: Canvas) {
 
+        //draw primary circle
+
        var h= this.height
         var w = this.width
         var ha = h/2f
@@ -46,10 +49,8 @@ class VolumeKnob(context: Context?, attrs: AttributeSet?) : View(context, attrs)
             }
        // radius -= radius/10f
         paint.style = Paint.Style.FILL
-        paint.color = Color.parseColor("#ffffff")
+        paint.color = resources.getColor(R.color.centercicle)
         canvas.drawCircle(wa , ha , radius, paint)
-        super.onDraw(canvas)
-
 
 
 
@@ -57,16 +58,45 @@ class VolumeKnob(context: Context?, attrs: AttributeSet?) : View(context, attrs)
         var wb = w/2f/2.1f
 
         if(wb < hb){
-            radius = wb / 5f
+            radius = wb / 3f
         }
         else{
-            radius = hb / 5f
+            radius = hb / 3f
         }
         // radius -= radius/10f
         paint.style = Paint.Style.FILL
-        paint.color = Color.parseColor("#00ff00")
-        canvas.drawCircle(wb , hb , radius, paint2)
+        paint.color = resources.getColor(R.color.lvloknob)
+        canvas.drawCircle(wb , hb , radius, paint)
 
+
+
+        var hc = h/2f/2.1f
+        var wc = w/2f/2.1f
+
+        if(wc < hc){
+            radius = wc / 4f
+        }
+        else{
+            radius = hc / 4f
+        }
+        // radius -= radius/10f
+        paint.style = Paint.Style.FILL
+        paint.color = resources.getColor(R.color.lvl1knob)
+        canvas.drawCircle(wc , hc, radius, paint)
+
+        var hd = h/2f/2.1f
+        var wd = w/2f/2.1f
+
+        if(wd < hd){
+            radius = wd / 6f
+        }
+        else{
+            radius = hd / 6f
+        }
+        // radius -= radius/10f
+        paint.style = Paint.Style.FILL
+        paint.color = resources.getColor(R.color.lvl2knob)
+        canvas.drawCircle(wd , hd, radius, paint)
         super.onDraw(canvas)
         //paint.color = Color.WHITE
        // canvas.drawPaint(paint)
