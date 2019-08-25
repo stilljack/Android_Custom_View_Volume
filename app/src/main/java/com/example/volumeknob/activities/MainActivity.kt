cc44cfb9914da05ab752a2919255e4bd9e58b481
stilljack <stilljack@gmail.comconfig --global user.email stilljack@gmail.com>
2019-08-25 15:13:26 -0700

    starting over

--- app/src/main/java/com/example/volumeknob/activities/MainActivity.kt
+++ app/src/main/java/com/example/volumeknob/activities/MainActivity.kt
@@ -5,17 +5,7 @@ import android.os.Bundle
 import com.example.volumeknob.R
 import kotlinx.android.synthetic.main.activity_main.*
 
-///we're gonna make two circles in a customView that will be a superset of Relative Layout
-//use sample code to rotate the indicator around the plate
-
-//.1. going to try to create the CustomView VolumeKnob as a superset of RelativeLayout consisting of 2 drawn circles
-
-//.2. oh wow how the time has flown. We have one circle succesfully displaying and a better idea of how to use canvas and drawing to it, w
-//relativeLAyout was replaced with view while we debuged. work continues
-
-//.3. installed some code from https://www.techotopia.com/index.php/Android_Touch_and_Multi-touch_Event_Handling to help understand the data
-// from ontouchlistener, going to alter the code to hopefully make real time+luid adjustment possible -- also adjust initial display so
-//knob spawns aproximately in the middle
+///starting over
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,33 +14,9 @@ class MainActivity : AppCompatActivity() {
 
 
 
-btn.setOnClickListener {
-    vk.rotation += 180f
-
-}
-
-
-
-        /* SAMPLE CODE
-        public boolean onTouchEvent(MotionEvent event) { // triggered each time the touch state changes
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN: // triggered when view is touched
-                // get and store start point with event.getX()
-                break;
-                case MotionEvent.ACTION_MOVE: // triggered after ACTION_DOWN but when touch is moved
-                // get end point and calculate total distance traveled
-                // use the total distance traveled to calculate the desired change in rotation
-                // apply that change to your rotation variable
-                // you may want to use a minimum and maximum rotation value to limit the rotation
-                // use the new rotation to convert to the desired volume setting
-                invalidate(); // this will cause the onDraw method to be called again with your new values
-                break;
-                case MotionEvent.ACTION_UP: // triggered when touch ends
-                break;
-            }
-            return true; // this indicates that the event has been processed
+        btn.setOnClickListener {
+            vk.rotation += 10f
         }
-        */
-    }
 
+    }
 }
--- app/src/main/java/com/example/volumeknob/customClasses/VolumeKnob.kt
+++ app/src/main/java/com/example/volumeknob/customClasses/VolumeKnob.kt
@@ -1,23 +1,23 @@
 package com.example.volumeknob.customClasses
 
-
-
-import android.annotation.SuppressLint
 import android.content.Context
+
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.Log
-import android.view.GestureDetector
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import android.view.MotionEvent
-import android.widget.TextView
-import com.example.volumeknob.R
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 
-@SuppressLint("ByteOrderMark")
+
+
+
+
 class VolumeKnob(context: Context?, attrs: AttributeSet?) : ImageView(context, attrs) {
+ //   constructor(context: Context) : this(context, null)
+
 /*companion object {
     fun rotate() {for (i in 0 until 10) {
         this.rotate(10f)
@@ -26,10 +26,6 @@ class VolumeKnob(context: Context?, attrs: AttributeSet?) : ImageView(context, a
 }*/
 
 
-    val mDetector = GestureDetector(context, GestureDetector.SimpleOnGestureListener())
-    private val paint = Paint()
-    private val paint2 = Paint()
-    private val paint3 = Paint()
 //constructor(context: Context): this(context, null)
     //
     // POSSIBLE ALTERNATE FOR VOLUMEKNOB(context: Context, attrs: AttributeSet, defStyleAttr : Int, defStyleRes: Int)
@@ -37,192 +33,126 @@ class VolumeKnob(context: Context?, attrs: AttributeSet?) : ImageView(context, a
     //(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes)
 //    constructor(context: Context): this(context, null)
 //    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
-//    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
-//    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes)
 
-
-    init {
-        invalidate();
-    }
-
-    fun handleTouch(m: MotionEvent) {
-        /*var textView1 = findViewById<TextView>(R.id.textView1)
-        var textView2 = findViewById<TextView>(R.id.textView2)*/
-
-        var pointerCount = m.getPointerCount()
-
-        for (i in 0 until pointerCount) {
-            var xa = m.getX(i)
-            var ya = m.getY(i)
-            id = m.getPointerId(i)
-            var action = m.getActionMasked()
-            var actionIndex = m.getActionIndex()
-            var actionString: String
-
-
-            when (action) {
-                MotionEvent.ACTION_DOWN ->
-                    actionString = "DOWN"
-
-                MotionEvent.ACTION_UP ->
-                    actionString = "UP"
-
-                MotionEvent.ACTION_POINTER_DOWN ->
-                    actionString = "PNTR DOWN"
-
-                MotionEvent.ACTION_POINTER_UP ->
-                    actionString = "PNTR UP"
-
-                MotionEvent.ACTION_MOVE ->
-                    actionString = "MOVE"
-
-                else ->
-                    actionString = ""
-            }
-
-
-            var touchStatus =
-                "Action: " + actionString + " Index: " + actionIndex + " ID: " + id + " X: " + xa + " Y: " + ya
-
-            if (id == 0) {
-                Log.i("$id","$touchStatus")
-            } else {
-                Log.i("$id","$touchStatus")
-
-
-        }
-        }
-    }
-
+    //    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes)
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+  private var downx:Float = 0f
+   private var movex:Float = 0f
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        super.onTouchEvent(event)
 
-        handleTouch(event)
+
         // triggered each time the touch state changes
-        var xtrack:Float
-        var ytrack:Float
-        var xmovetrack:Float
-        var ymovetrack:Float
-        var xautrack:Float
-        var yautrack:Float
-
-        var axisdownvalue:Float
-        var axismovevalue:Float
-        var axisupvalue:Float
-
-        var xdiff:Float
-       var ydiff:Float
-
-
         when (event.action) {
-           MotionEvent.ACTION_DOWN // triggered when view is touched
-            -> { Log.i("ACTION","down${event.actionIndex}")
-                event.actionIndex.toString()
-                xtrack= event.x
-               ytrack=event.y
-
-
-
-               return true
+            MotionEvent.ACTION_DOWN // triggered when view is touched
+            -> {
+            downx= event.x
             }
-            MotionEvent.ACTION_MOVE -> {Log.i("ACTION","move${event.actionIndex}")
-                xmovetrack= event.x
-                ymovetrack=event.y
+            MotionEvent.ACTION_MOVE // triggered after ACTION_DOWN but when touch is moved
+            -> {
+
+                movex = event.x
+
+
+
 
                 // get end point and calculate total distance traveled
                 // use the total distance traveled to calculate the desired change in rotation
                 // apply that change to your rotation variable
                 // you may want to use a minimum and maximum rotation value to limit the rotation
                 // use the new rotation to convert to the desired volume setting
-                invalidate() // this will cause the onDraw method to be called again with your new values
-                return true}
-            MotionEvent.ACTION_UP -> {Log.i("ACTION","up${event.actionIndex}")
-                xautrack=event.x
-                yautrack=event.y
-
-                return true
+                // this will cause the onDraw method to be called again with your new values
+            }
+            MotionEvent.ACTION_UP // triggered when touch ends
+            -> {
             }
         }// get and store start point with event.getX()
-        return true; // this indicates that the event has been processed
-    }
-    override fun onDraw(canvas: Canvas) {
-
-        //draw primary circle
-
-       var h= this.height
-        var w = this.width
-        var ha = h/2f
-        var wa = w/2f
-        var radius:Float
-        radius = if(width <= height){
-            width / 2f
-        } else{
-            height / 2f
-        }
-       // radius -= radius/10f
-        paint.style = Paint.Style.FILL
-        paint.color = resources.getColor(R.color.centercicle)
-        canvas.drawCircle(wa , ha , radius, paint)
-
-
-
-        var hb = h/2f/2.1f
-        var wb = w/2f/2.1f
-
-        if(wb <= hb){
-            radius = wb / 3f
-        }
-        else{
-            radius = hb / 3f
-        }
-        // radius -= radius/10f
-        paint.style = Paint.Style.FILL
-        paint.color = resources.getColor(R.color.lvloknob)
-        canvas.drawCircle(wb , hb , radius, paint)
-
-
-
-        var hc = h/2f/2.1f
-        var wc = w/2f/2.1f
-
-        radius = if(wc <= hc){
-            wc / 4f
-        } else{
-            hc / 4f
-        }
-        // radius -= radius/10f
-        paint.style = Paint.Style.FILL
-        paint.color = resources.getColor(R.color.lvl1knob)
-        canvas.drawCircle(wc , hc, radius, paint)
-
-        var hd = h/2f/2.1f
-        var wd = w/2f/2.1f
-
-        radius = if(wd < hd){
-            wd / 6f
-        } else{
-            hd / 6f
-        }
-        // radius -= radius/10f
-        paint.style = Paint.Style.FILL
-        paint.color = resources.getColor(R.color.lvl2knob)
-
-        canvas.drawCircle(wd , hd, radius, paint)
-
-
-        //paint.color = Color.WHITE
-       // canvas.drawPaint(paint)
-        // Use Color.parseColor to define HTML colors
-        //supposedly the following formula leads to super good circles
-       // width/2-mBitmap.getWidth()/2, height/2-mBitmap.getHeight()/2
-
-
-        //initial rotate getting us to starting at max
-        this.rotation += 45f
-        super.onDraw(canvas)
+        return true // this indicates that the event has been processed
     }
 
-}
+
+
+    init {
+
+    }
+     override fun onDraw(canvas: Canvas) {
+
+
+
+         //draw primary circle
+
+           var h = this.height
+           var w = this.width
+           var ha = h / 2f
+           var wa = w / 2f
+           var radius: Float
+           radius = if (width <= height) {
+               width / 2f
+           } else {
+               height / 2f
+           }
+           // radius -= radius/10f
+           paint.style = Paint.Style.FILL
+           paint.color =ContextCompat.getColor(context, com.example.volumeknob.R.color.centercicle)
+
+         canvas.drawCircle(wa, ha, radius, paint)
+
+
+        var hb = h / 2f / 2.1f
+           var wb = w / 2f / 2.1f
+
+           if (wb <= hb) {
+               radius = wb / 3f
+           } else {
+               radius = hb / 3f
+           }
+           // radius -= radius/10f
+         paint.color =ContextCompat.getColor(context, com.example.volumeknob.R.color.lvloknob)
+           canvas.drawCircle(wb, hb, radius, paint)
+
+
+           var hc = h / 2f / 2.1f
+           var wc = w / 2f / 2.1f
+
+           radius = if (wc <= hc) {
+               wc / 4f
+           } else {
+               hc / 4f
+           }
+           // radius -= radius/10f
+           paint.color = ContextCompat.getColor(context, com.example.volumeknob.R.color.lvloknob)
+           canvas.drawCircle(wc, hc, radius, paint)
+
+           var hd = h / 2f / 2.1f
+           var wd = w / 2f / 2.1f
+
+           radius = if (wd < hd) {
+               wd / 6f
+           } else {
+               hd / 6f
+           }
+           // radius -= radius/10f
+
+           paint.color = ContextCompat.getColor(context, com.example.volumeknob.R.color.lvl2knob)
+           canvas.drawCircle(wd, hd, radius, paint)
+
+
+           //paint.color = Color.WHITE
+           //canvas.drawPaint(paint)
+           // Use Color.parseColor to define HTML colors
+           //supposedly the following formula leads to super good circles
+           // width/2-mBitmap.getWidth()/2, height/2-mBitmap.getHeight()/2
+
+
+           //initial rotate getting us to starting at max
+           this.rotation += 40f
+         paint.color = Color.MAGENTA
+         //canvas?.drawCircle(width/2.0f, height/2f, 500f, paint)
+
+           super.onDraw(canvas)
+       }
+   }
+
+
 
 
 
